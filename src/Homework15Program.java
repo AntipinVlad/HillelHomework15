2fea993feac6a55a4eab94279e8e3808f840e15b
AntipinVlad <antipinmail1@gmail.com>
2024-01-21 12:49:17 +0200

    Homework15

---
+++ src/Homework15Program.java
@@ -0,0 +1,9 @@
+public class Homework15Program {
+    public static void main(String[] args) {
+        Person person = new Person();
+
+        System.out.println(person.personInfo("Will", "Smith", "New York", "2936729462846"));
+        System.out.println(person.personInfo("Jackie", "Chan", "Shanghai", "12312412412"));
+        System.out.println(person.personInfo("Sherlock", "Holmes", "London", "37742123513"));
+    }
+}
---
+++ src/Person.java
@@ -0,0 +1,5 @@
+public class Person {
+    public String personInfo(String name, String surName, String city, String phoneNumber) {
+        return "To contact " + name + " " + surName + " from " + city + " city, please call " + phoneNumber;
+    }
+}
